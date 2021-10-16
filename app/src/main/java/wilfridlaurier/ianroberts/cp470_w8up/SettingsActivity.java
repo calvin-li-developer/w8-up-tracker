520c1d156265c35aad7408d6dc87de2dc4781bab
Keadon2022 <78323297+Keadon2022@users.noreply.github.com>
2021-10-16 12:41:48 -0400

    Created activity files

---
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/SettingsActivity.java
@@ -0,0 +1,14 @@
+package wilfridlaurier.ianroberts.cp470_w8up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/SocialMediaActivity.java
@@ -0,0 +1,14 @@
+package wilfridlaurier.ianroberts.cp470_w8up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+    }
+}
