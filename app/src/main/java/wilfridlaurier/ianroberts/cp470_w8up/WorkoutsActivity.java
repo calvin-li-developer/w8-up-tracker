ade193d91def0a6746cc362e92acca02fef69467
Ian R <IanR9532@gmail.com>
2021-10-16 13:01:05 -0400

    First new branch commit

--- app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/WorkoutsActivity.java
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/WorkoutsActivity.java
@@ -10,5 +10,6 @@ public class WorkoutsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts);
+        //New branch test
     }
 }
