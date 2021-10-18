c03567db35e7e89907fb29e5a8544670369aad83
scoban007 <scoban888@gmail.com>
2021-10-18 18:53:00 -0400

    Iteration one

--- app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/HabitTrackerActivity.java
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/HabitTrackerActivity.java
@@ -2,7 +2,11 @@ package wilfridlaurier.ianroberts.cp470_w8up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HabitTrackerActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class HabitTrackerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_tracker);
+        ImageButton hydrationButton = findViewById(R.id.hydration);
+        hydrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HabitTrackerActivity.this, HydrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
