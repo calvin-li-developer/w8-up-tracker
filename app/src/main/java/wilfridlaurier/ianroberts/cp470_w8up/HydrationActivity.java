f221a13d0f2f13cc2ba25fb1df18fd23d5c9cd4c
scoban007 <scoban888@gmail.com>
2021-12-07 16:23:42 -0500

    Scoban merge Dec 07

--- app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/HydrationActivity.java
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/HydrationActivity.java
@@ -3,12 +3,59 @@ package wilfridlaurier.ianroberts.cp470_w8up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class HydrationActivity extends AppCompatActivity {
+
+    Button addWater;
+    EditText waterText;
+    Button waterGoal;
+    EditText goalText;
+    ProgressBar waterBar;
+    String waterTextVal;
+    String goalTextVal;
+    double total;
+    double goal;
+    double progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hydration);
+
+        addWater = findViewById(R.id.addWater);
+        waterText = findViewById(R.id.waterText);
+        waterGoal = findViewById(R.id.waterGoal);
+        goalText = findViewById(R.id.goalText);
+        waterBar =  findViewById(R.id.waterBar);
+        waterTextVal = waterText.getText().toString();
+        total = Integer.parseInt(waterTextVal);
+        goalTextVal = goalText.getText().toString();
+        goal = Integer.parseInt(goalTextVal);
+
+        waterBar.setProgress((int)(total/goal)*100);
+
+        addWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                total += Integer.parseInt(waterText.getText().toString());
+                Log.i("HydrationActivity", "" + (int)total);
+                progress = total/goal;
+                Log.i("HydrationActivity", "" + (int)((progress)*100));
+                waterBar.setProgress((int)(progress*100));
+            }
+        });
+
+        waterGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goal = Integer.parseInt(goalText.getText().toString());
+                waterBar.setProgress((int)(total/goal)*100);
+            }
+        });
     }
 }
---
+++ app/src/main/java/wilfridlaurier/ianroberts/cp470_w8up/SetReminderActivity.java
@@ -0,0 +1,87 @@
+package wilfridlaurier.ianroberts.cp470_w8up;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class SetReminderActivity extends AppCompatActivity {
+
+    EditText title;
+    TextView time;
+    TextView date;
+    Button timeSet;
+    Button dateSet;
+    TimePicker chooseTime;
+    DatePicker chooseDate;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_reminder);
+
+        title = findViewById(R.id.title);
+        time = findViewById(R.id.timeSet);
+        date = findViewById(R.id.dateSet);
+        timeSet = findViewById(R.id.timeSetButton);
+        dateSet = findViewById(R.id.dateSetButton);
+        chooseTime = findViewById(R.id.chooseTime);
+        chooseDate = findViewById(R.id.chooseDate);
+
+        Date currDateTime = Calendar.getInstance().getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM/dd/yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+
+        time.setText(timeFormat.format(currDateTime));
+        date.setText(dateFormat.format(currDateTime));
+
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseDate.setVisibility(View.INVISIBLE);
+                chooseTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        dateSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseTime.setVisibility(View.INVISIBLE);
+                chooseDate.setVisibility(View.VISIBLE);
+            }
+        });
+
+        chooseTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                Time newTime = new Time(i, i1, 0);
+                time.setText(timeFormat.format(newTime));
+            }
+        });
+
+        chooseDate.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                Calendar cal = Calendar.getInstance();
+                cal.set(i, i1, i2);
+                date.setText(dateFormat.format(cal.getTime()));
+            }
+        });
+
+    }
+
+}
